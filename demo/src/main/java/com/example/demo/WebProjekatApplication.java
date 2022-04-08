e442dc30dbeb2c29529a0d023b2b8ebbb5c359c3
Luka Stajic <luka2001stajic@gmail.com>
2022-04-08 12:00:05 +0200

    Kreiranje projekta sa springboot-om

---
+++ demo/src/main/java/com/example/demo/WebProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjekatApplication.class, args);
+	}
+
+}
